08a0042819577422b830e8e474aab6c8d9578569
OCapde <157165535+OCapde@users.noreply.github.com>
2024-01-19 23:15:43 +0100

    Update contador4.php

--- contador4.php
+++ contador4.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $servername = "localhost"; 
-$username = "admin"; 
+$username = "root"; 
 $password = "Monti2021"; 
 $dbname = "estadistics"; 
 
